235a4101c138a8734b4d6d5b4e7ee9c7de446e10
Shubham28497 <74322538+Shubham28497@users.noreply.github.com>
2023-08-16 12:00:03 +0530

    add some css

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 const Product = () => {
   const [products, getProducts] = useState([]);
@@ -11,33 +11,32 @@ const Product = () => {
       .then((result) => getProducts(result));
   }, []);
   const cards = products.map((product) => (
-    <div className="col-md-3">
-      <Card style={{ width: "18rem" }}>
+    <div className="col-md-3" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
         <div className="text-center">
-        <Card.Img
-          variant="top"
-          src={product.image}
-          style={{ width: "100px", height: "130px" }}
-        />
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
         </div>
-        
+
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>INR: {product.price}</Card.Text>
         </Card.Body>
+        <Card.Footer style={{background:'white'}}>
+          <Button variant="primary">Add to cart</Button>
+        </Card.Footer>
       </Card>
     </div>
-  )) 
+  ));
   return (
     <>
       <h1>Product Dashboard</h1>
       <div className="row">{cards}</div>
     </>
-  )
+  );
 };
 
 export default Product;
