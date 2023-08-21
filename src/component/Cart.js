facb3f6145e049c4b1f3434052563a2c1609a3c2
Shubham28497 <74322538+Shubham28497@users.noreply.github.com>
2023-08-21 10:03:11 +0530

    updated remove item from cart

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,8 +1,41 @@
-const Cart=()=>{
-    return(
-        <>
-        <h1>Cart</h1>
-        </>
-    )
-}
-export default Cart
+import { useSelector } from "react-redux";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { useDispatch } from "react-redux";
+import { remove } from "../store/CartSlice";
+const Cart = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.cart);
+  const removeToCart = (id) => {
+    dispatch(remove(id));
+  };
+  const cards = products.map((product) => (
+    <div className="col-md-12" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>INR: {product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: "white" }}>
+          <Button variant="danger" onClick={() => removeToCart(product.id)}>
+            Remove Item
+          </Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+  return (
+    <>
+      <div className="row">{cards}</div>
+    </>
+  );
+};
+export default Cart;
--- src/store/CartSlice.js
+++ src/store/CartSlice.js
@@ -9,10 +9,13 @@ const cartSlice= createSlice({
     reducers:{
     add(state,action){
         state.push(action.payload)
+    },
+    remove(state,action){
+        return state.filter(item=>item.id!==action.payload)
     }
 
     }
     
 })
-export const {add}= cartSlice.actions // actions gives the function like add
+export const {add,remove}= cartSlice.actions // actions gives the function like add
 export default cartSlice.reducer  // reducers gives the state
